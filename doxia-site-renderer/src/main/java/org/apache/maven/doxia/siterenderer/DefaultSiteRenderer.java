8b13618e1c83a31acc482bf74e4c0477e5520f6c
Michael Osipov <michaelo@apache.org>
2023-03-19 18:17:41 +0100

    Partially revert c2f7271ff75c42ede2a3e0ddb205293252cd8ddb

--- doxia-site-renderer/src/main/java/org/apache/maven/doxia/siterenderer/DefaultSiteRenderer.java
+++ doxia-site-renderer/src/main/java/org/apache/maven/doxia/siterenderer/DefaultSiteRenderer.java
@@ -485,9 +485,7 @@ public class DefaultSiteRenderer implements Renderer {
         context.put("locale", siteRenderingContext.getLocale());
         context.put("supportedLocales", Collections.unmodifiableList(siteRenderingContext.getSiteLocales()));
 
-        if (siteRenderingContext.getPublishDate() != null) {
-            context.put("publishDate", siteRenderingContext.getPublishDate());
-        }
+        context.put("publishDate", siteRenderingContext.getPublishDate());
 
         if (DOXIA_SITE_RENDERER_VERSION != null) {
             context.put("doxiaSiteRendererVersion", DOXIA_SITE_RENDERER_VERSION);
