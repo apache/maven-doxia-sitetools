7b4d57238e7242c48ea77f7564414e9e4bf6a128
John Dimeo <jdimeo@us.ci.org>
2024-07-26 13:53:30 -0400

    Fix from merge

--- doxia-site-renderer/src/main/java/org/apache/maven/doxia/siterenderer/DefaultSiteRenderer.java
+++ doxia-site-renderer/src/main/java/org/apache/maven/doxia/siterenderer/DefaultSiteRenderer.java
@@ -140,36 +140,6 @@ public class DefaultSiteRenderer implements Renderer {
     private static final String SKIN_TEMPLATE_LOCATION = "META-INF/maven/site.vm";
 
     private static final String TOOLS_LOCATION = "META-INF/maven/site-tools.xml";
-    
-    private static final String DOXIA_SITE_RENDERER_VERSION;
-    
-    static
-    {
-        InputStream inputStream = DefaultSiteRenderer.class.getResourceAsStream( "/META-INF/"
-                + "maven/org.apache.maven.doxia/doxia-site-renderer/pom.properties" );
-        
-        String version = null;
-        if ( inputStream == null )
-        {
-            LOGGER.debug( "pom.properties for doxia-site-renderer could not be found." );
-        }
-        else
-        {
-            Properties properties = new Properties();
-            try ( InputStream in = inputStream )
-            {
-                properties.load( in );
-                version = properties.getProperty( "version" );
-            }
-            catch ( IOException e )
-            {
-                LOGGER.debug( "Failed to load pom.properties, so doxiaSiteRendererVersion is not available"
-                        + " in the Velocity context." );
-            }
-        }
-        
-        DOXIA_SITE_RENDERER_VERSION = version;
-    }
 
     private static final String DOXIA_SITE_RENDERER_VERSION = getSiteRendererVersion();
 
